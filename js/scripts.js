51c8e7bd5e52dcebda72b653a9cf5f2169986d7a
Adriana C and Susan Southall <adriana@>
2018-04-02 10:47:47 -0700

    make address book

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+// get userinput , output the fullname, clear values
+// click fullname, show contact detail
+// click add address, show another city, state, street
+
+
+
+$().ready(function(){
+  $(".address-form").submit(function(e){
+    e.preventDefault();
+    // debugger;
+    let inputKeys = ['firstname', 'lastname', 'street', 'city', 'state'];
+    var inputObjs = {};
+    let inputValues = document.querySelectorAll("input");
+    for(let i = 0; i < inputValues.length; i++){
+      inputObjs[inputKeys[i]] = inputValues[i].value;
+      $(".detail").append("<p class='lead'>" + inputKeys[i] + ": " + inputValues[i].value + "</p>" );
+    }
+    $(".fullname").append("<p class='lead mt-4'><a href='#1'>" + inputObjs.firstname[0].toUpperCase()+inputObjs.firstname.substring(1) + " " + inputObjs.lastname+ "</a></p>")
+
+  });
+
+  $(".fullname").click(function(){
+    $(".detail").show();
+  });
+
+
+  $("#reset").click(function(){
+      window.location.reload();
+  });
+
+
+});
