ca41d56c6924cfde387a658206ca69c4d46f9517
Adrianacmy <adrianamullaney@gmail.com>
2018-04-02 21:47:05 -0700

    refactor with class

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,93 @@
 // get userinput , output the fullname, clear values
 // click fullname, show contact detail
 // click add address, show another city, state, street
-class Contact{
-	constructor(first, last){
+
+// business logic
+class Contact {
+	constructor(first, last) {
 		this.first = first;
 		this.last = last;
 		this.addresses = [];
 	}
+
+	fullName() {
+		return this.capitalize(this.first) + " " + this.capitalize(this.last);
+	}
+
+	capitalize(aString) {
+		return aString.charAt(0).toUpperCase() + aString.slice(1).toLowerCase();
+	}
 }
 
+
 class Address {
-	constructor(street, city, state){
+	constructor(street='', city='', state='') {
 		this.street = street;
 		this.city = city;
 		this.state = state;
 	}
+
+	fullAddress(){
+		return `${this.street}, ${this.city}, ${this.state}`;
+	}
 }
 
+
+// user interface logic
 $().ready(function () {
 	$(".address-form").submit(function (e) {
 		e.preventDefault();
 		// debugger;
-		let inputKeys = ['firstname', 'lastname', 'street', 'city', 'state'];
-		var inputObjs = {};
-		let inputValues = document.querySelectorAll("input");
-		for (let i = 0; i < inputValues.length; i++) {
-			inputObjs[inputKeys[i]] = inputValues[i].value;
-			$(".detail").append("<p class='lead'>" + inputKeys[i] + ": " + inputValues[i].value + "</p>");
-		}
-		$(".fullname").append("<p class='lead mt-4'><a href='#1'>" + inputObjs.firstname[0].toUpperCase() + inputObjs.firstname.substring(1) + " " + inputObjs.lastname + "</a></p>")
 
+		let firstname = $(".firstname").val();
+		let lastname = $(".lastname").val();
+		let contact = new Contact(firstname, lastname);
+		let newAddress = new Address();
+		$(".new-address").each(function () {
+			let street = $(this).find("input.street").val();
+			let city = $(this).find("input.city").val();
+			let state = $(this).find("input.state").val();
+			newAddress = new Address(street, city, state);
+			contact.addresses.push(newAddress)
+		});
+		$(".fullname").append(`<p class='lead ${contact.fullName()}'><a href="#">Full Name: ${contact.fullName()} </a></p>`);
+		$(".fullname").click(function () {
+			$(".detail").show();
+			contact.addresses.forEach(function (address) {
+				$(".detail").append(`
+															<p class='lead'> First Name: ${contact.capitalize(contact.first)}</p>
+															<p class='lead'> Last Name: ${contact.capitalize(contact.last)}</p>
+															<p class='lead'>Address: ${newAddress.fullAddress()}</p>
+														`);
+			});
+		});
+		$("input").val("");
+		$("#reset").click(function () {
+			window.location.reload();
+		});
 	});
 
-	$(".fullname").click(function () {
-		$(".detail").show();
+	// add new address
+	$(".add-address").click(function () {
+		let newAddress = `
+		<div class='new-address'>
+			<div class="col-12">
+				<div class="form-group">
+					<input type="text" class="form-control street" placeholder="street" required>
+				</div>
+			</div>
+			<div class="col-12">
+				<div class="form-group">
+					<input type="text" class="form-control city" placeholder="city" required>
+				</div>
+			</div>
+			<div class="col-12">
+				<div class="form-group">
+					<input type="text" class="form-control state" placeholder="state" required>
+				</div>
+			</div>
+		</div>`;
+		$("#new-address").append(newAddress);
 	});
-
-
-	$("#reset").click(function () {
-		window.location.reload();
-	});
-
 
 });
