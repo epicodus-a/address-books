b99a6ead6d9f02487a02aea7d78c2ffb9c670c22
Adrianacmy <adrianamullaney@gmail.com>
2018-04-02 20:03:53 -0700

    refactor with class

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,45 @@
 // get userinput , output the fullname, clear values
 // click fullname, show contact detail
 // click add address, show another city, state, street
+class Contact{
+	constructor(first, last){
+		this.first = first;
+		this.last = last;
+		this.addresses = [];
+	}
+}
+
+class Address {
+	constructor(street, city, state){
+		this.street = street;
+		this.city = city;
+		this.state = state;
+	}
+}
+
+$().ready(function () {
+	$(".address-form").submit(function (e) {
+		e.preventDefault();
+		// debugger;
+		let inputKeys = ['firstname', 'lastname', 'street', 'city', 'state'];
+		var inputObjs = {};
+		let inputValues = document.querySelectorAll("input");
+		for (let i = 0; i < inputValues.length; i++) {
+			inputObjs[inputKeys[i]] = inputValues[i].value;
+			$(".detail").append("<p class='lead'>" + inputKeys[i] + ": " + inputValues[i].value + "</p>");
+		}
+		$(".fullname").append("<p class='lead mt-4'><a href='#1'>" + inputObjs.firstname[0].toUpperCase() + inputObjs.firstname.substring(1) + " " + inputObjs.lastname + "</a></p>")
+
+	});
+
+	$(".fullname").click(function () {
+		$(".detail").show();
+	});
 
 
-
-$().ready(function(){
-  $(".address-form").submit(function(e){
-    e.preventDefault();
-    // debugger;
-    let inputKeys = ['firstname', 'lastname', 'street', 'city', 'state'];
-    var inputObjs = {};
-    let inputValues = document.querySelectorAll("input");
-    for(let i = 0; i < inputValues.length; i++){
-      inputObjs[inputKeys[i]] = inputValues[i].value;
-      $(".detail").append("<p class='lead'>" + inputKeys[i] + ": " + inputValues[i].value + "</p>" );
-    }
-    $(".fullname").append("<p class='lead mt-4'><a href='#1'>" + inputObjs.firstname[0].toUpperCase()+inputObjs.firstname.substring(1) + " " + inputObjs.lastname+ "</a></p>")
-
-  });
-
-  $(".fullname").click(function(){
-    $(".detail").show();
-  });
-
-
-  $("#reset").click(function(){
-      window.location.reload();
-  });
+	$("#reset").click(function () {
+		window.location.reload();
+	});
 
 
 });
